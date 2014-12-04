126f73c02433ca11c77ffb9f01d8a1e1956197a6
Terry-Shi <terryemailbox@gmail.com>
2014-12-05 02:31:01 +0800

    init commit

---
+++ src/main/java/com/myweka/KMeans.java
@@ -0,0 +1,54 @@
+package com.myweka;
+
+import java.io.File;
+import java.io.Reader;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import weka.clusterers.ClusterEvaluation;
+import weka.clusterers.SimpleKMeans;
+import weka.core.DistanceFunction;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.core.EuclideanDistance;
+
+
+public class KMeans {
+    
+    public static void main(String[] args) {
+    	try {
+    	
+	    	ArffLoader loader = new ArffLoader();
+	    	loader.setFile(new File("data/kmeans.arff"));
+	    	
+	    	Instances data =  loader.getDataSet();
+	    	data.deleteStringAttributes();
+	    	SimpleKMeans simpleKMeans = new SimpleKMeans();
+	    	// 设置聚类要得到的类别数量
+	    	simpleKMeans.setNumClusters(3);
+	    	simpleKMeans.setDontReplaceMissingValues(true);
+	    	
+	    	// 函数设置聚类算法内部的距离计算方式
+	    	DistanceFunction m_DistanceFunction = new EuclideanDistance();
+	    	simpleKMeans.setDistanceFunction(m_DistanceFunction);//default: weka.core.EuclideanDistance
+	    	simpleKMeans.setMaxIterations(10);
+	    	
+	    	// 使用聚类算法对样本进行聚类
+	    	simpleKMeans.buildClusterer(data);
+	    	
+	    	Instances tempIns = simpleKMeans.getClusterCentroids();
+	        System.out.println("聚类中心:");
+	        System.out.println("CentroIds: " + tempIns);
+	        
+	        Instance ins = new Instance(data.firstInstance());
+	    	int i = simpleKMeans.clusterInstance(ins);
+	    	System.out.println("China Football is level " + i);
+	    	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
+}
